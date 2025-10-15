9a64dab9dd34c88a17541b3fe7ad27c3099c5059
suhailskx7-ops <suhailsk.x7@gmail.com>
2025-10-16 02:43:12 +0530

    terraform applied

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,7 @@
-variable "aws_region" {
-  default = "us-east-1"
+output "cluster_name" {
+  value = module.eks.cluster_id
 }
 
-variable "project_name" {
-  default = "hello-web"
-}
-
-variable "vpc_id" {
-  description = "VPC ID where EKS will be deployed"
-}
-
-variable "subnets" {
-  description = "List of subnet IDs for EKS nodes"
-  type        = list(string)
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,10 @@
+aws_region   = "us-east-1"
+project_name = "hello-web"
+
+vpc_id = "vpc-0b5ee2cba3cf34e33"
+
+subnets = [
+  "subnet-0d5ad8349bc02f09d",
+  "subnet-0e0a6d4c5b58ce7e0",
+  "subnet-0fda8681836b5454d"
+]
